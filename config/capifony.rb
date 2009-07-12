e956a64476125cf00ad0769e776626bb2595fc50
ever.zet <ever.zet@gmail.com>
2009-07-12 10:30:46 +0300

    1st working version

--- config/capifony.rb
+++ config/capifony.rb
@@ -1,23 +1,36 @@
 # setting symfony paths:
-set :shared_children,   %w(config log cache web/uploads)
+set :shared_children,   %w(log web/uploads)
 set :release_children,  shared_children
-set :assets_children,   %s(css images js)
+set :assets_children,   %w(css images js)
 
 namespace :deploy do
+  task :setup, :except => { :no_release => true } do
+    dirs = [deploy_to, releases_path, shared_path]
+    dirs += shared_children.map { |d| File.join(shared_path, d) }
+    run "mkdir -p #{dirs.join(' ')} && chmod g+w #{dirs.join(' ')}"
+  end
+
   task :finalize_update, :except => { :no_release => true } do
     run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
 
     # getting paths
     shared_paths  = shared_children.map { |d| File.join(shared_path, d) }
-    release_paths = release_children.map { |d| File.join(latest_release, d) }
+    release_paths = release_children.map { |d| File.join(latest_release + '/', d) }
 
     # removing unnecessary paths
     run "rm -rf #{release_paths.join(' ')}"
 
     # creating symlinks
-    shared_paths.each_index do |i|
-      run "ln -s #{shared_path.at(i)} #{release_paths.at(i)}"
-    end
+    shared_paths.each_index { |i| run "ln -s #{shared_paths.at(i)} #{release_paths.at(i)}" }
+
+    # cache directory
+    run "mkdir -p #{latest_release}/cache"
+
+    # publishing plugin assets
+    run "cd #{latest_release} && symfony plugin:publish-assets"
+
+    # permission
+    run "cd #{latest_release} && symfony project:permissions"
 
     if fetch(:normalize_asset_timestamps, true)
       stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
@@ -27,7 +40,12 @@ namespace :deploy do
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
-    # clearing symfony cache
-    try_runner "#{current_path}symfony cc"
+    # doing nothing. Overload this method if you need some custom actions
+  end
+end
+
+namespace :symfony do
+  def run_task task
+    run "cd #{current_path} && symfony #{task}"
   end
 end

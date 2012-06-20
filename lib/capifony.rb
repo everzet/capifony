b326293b609dda2d66200b299b4b3d3075de7a19
William DURAND <william.durand1@gmail.com>
2012-06-21 00:43:18 +0200

    Override deploy:setup to avoid creation of useless directories

--- lib/capifony.rb
+++ lib/capifony.rb
@@ -23,4 +23,22 @@ namespace :deploy do
 
   desc "Overwrite the stop task because symfony doesn't need it."
   task :stop do ; end
+
+  desc <<-DESC
+    Prepares one or more servers for deployment. Before you can use any \
+    of the Capistrano deployment tasks with your project, you will need to \
+    make sure all of your servers have been prepared with `cap deploy:setup'. When \
+    you add a new server to your cluster, you can easily run the setup task \
+    on just that server by specifying the HOSTS environment variable:
+
+      $ cap HOSTS=new.server.com deploy:setup
+
+    It is safe to run this task on servers that have already been set up; it \
+    will not destroy any deployed revisions or data.
+  DESC
+  task :setup, :except => { :no_release => true } do
+    dirs = [deploy_to, releases_path, shared_path]
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')}"
+    run "#{try_sudo} chmod g+w #{dirs.join(' ')}" if fetch(:group_writable, true)
+  end
 end

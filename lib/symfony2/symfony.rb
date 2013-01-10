541a2f1f75d0b913c272952ef50528b4f66310d0
Richard Fullmer <richardfullmer@gmail.com>
2013-01-09 17:00:47 -0800

    Clear controllers should only run on the app role

--- lib/symfony2/symfony.rb
+++ lib/symfony2/symfony.rb
@@ -192,7 +192,7 @@ namespace :symfony do
 
   namespace :project do
     desc "Clears all non production environment controllers"
-    task :clear_controllers do
+    task :clear_controllers, :roles => :app, :except => { :no_release => true } do
       capifony_pretty_print "--> Clear controllers"
 
       command = "#{try_sudo} sh -c 'cd #{latest_release} && rm -f"

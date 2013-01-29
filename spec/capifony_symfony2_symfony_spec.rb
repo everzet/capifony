8652e4ab1cfcf2bfcec53670e88139630af3d8c4
Martin Janser <martin@gogan.ch>
2013-01-29 22:02:56 +0100

    Add PHP options "detect_unicode" and "allow_url_fopen" to composer calls
    Composer requires the option "detect_unicode" disabled and the option
    "allow_url_fopen" enabled. These options can be passed at execution time
    to PHP.
    See: https://github.com/josegonzalez/homebrew-php/pull/83

--- spec/capifony_symfony2_symfony_spec.rb
+++ spec/capifony_symfony2_symfony_spec.rb
@@ -161,8 +161,8 @@ describe "Capifony::Symfony2 - symfony" do
     end
 
     it { should_not have_run('vendorDir=/var/www/current/vendor; if [ -d $vendorDir ] || [ -h $vendorDir ]; then cp -a $vendorDir /var/www/releases/20120927/vendor; fi;') }
-    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php\'') }
-    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php composer.phar update --no-scripts --verbose --prefer-dist\'') }
+    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php -d detect_unicode=Off -d allow_url_fopen=On\'') }
+    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php -d detect_unicode=Off -d allow_url_fopen=On composer.phar update --no-scripts --verbose --prefer-dist\'') }
   end
 
   context "when running symfony:composer:update with a given composer_bin" do
@@ -172,7 +172,7 @@ describe "Capifony::Symfony2 - symfony" do
     end
 
     it { should_not have_run('vendorDir=/var/www/current/vendor; if [ -d $vendorDir ] || [ -h $vendorDir ]; then cp -a $vendorDir /var/www/releases/20120927/vendor; fi;') }
-    it { should_not have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php\'') }
+    it { should_not have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php -d detect_unicode=Off -d allow_url_fopen=On\'') }
     it { should have_run(' sh -c \'cd /var/www/releases/20120927 && my_composer update --no-scripts --verbose --prefer-dist\'') }
   end
 
@@ -193,9 +193,9 @@ describe "Capifony::Symfony2 - symfony" do
 
     it { should_not have_run('vendorDir=/var/www/current/vendor; if [ -d $vendorDir ] || [ -h $vendorDir ]; then cp -a $vendorDir /var/www/releases/20120927/vendor; fi;') }
     it { should have_run(' sh -c \'cp /var/www/releases/20120920/composer.phar /var/www/releases/20120927/\'') }
-    it { should_not have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php\'') }
-    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php composer.phar self-update\'') }
-    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php composer.phar install --no-scripts --verbose --prefer-dist\'') }
+    it { should_not have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php -d detect_unicode=Off -d allow_url_fopen=On\'') }
+    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php -d detect_unicode=Off -d allow_url_fopen=On composer.phar self-update\'') }
+    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php -d detect_unicode=Off -d allow_url_fopen=On composer.phar install --no-scripts --verbose --prefer-dist\'') }
   end
 
   context "when running symfony:composer:install without any existing composer.phar in the previous release" do
@@ -206,9 +206,9 @@ describe "Capifony::Symfony2 - symfony" do
 
     it { should_not have_run('vendorDir=/var/www/current/vendor; if [ -d $vendorDir ] || [ -h $vendorDir ]; then cp -a $vendorDir /var/www/releases/20120927/vendor; fi;') }
     it { should_not have_run(' sh -c \'cp /var/www/releases/20120920/composer.phar /var/www/releases/20120927/\'') }
-    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php\'') }
-    it { should_not have_run(' sh -c \'cd /var/www/releases/20120927 && php composer.phar self-update\'') }
-    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php composer.phar install --no-scripts --verbose --prefer-dist\'') }
+    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php -d detect_unicode=Off -d allow_url_fopen=On\'') }
+    it { should_not have_run(' sh -c \'cd /var/www/releases/20120927 && php -d detect_unicode=Off -d allow_url_fopen=On composer.phar self-update\'') }
+    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php -d detect_unicode=Off -d allow_url_fopen=On composer.phar install --no-scripts --verbose --prefer-dist\'') }
   end
 
   context "when running symfony:composer:install" do
@@ -217,8 +217,8 @@ describe "Capifony::Symfony2 - symfony" do
     end
 
     it { should_not have_run('vendorDir=/var/www/current/vendor; if [ -d $vendorDir ] || [ -h $vendorDir ]; then cp -a $vendorDir /var/www/releases/20120927/vendor; fi;') }
-    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php\'') }
-    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php composer.phar install --no-scripts --verbose --prefer-dist\'') }
+    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php -d detect_unicode=Off -d allow_url_fopen=On\'') }
+    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php -d detect_unicode=Off -d allow_url_fopen=On composer.phar install --no-scripts --verbose --prefer-dist\'') }
   end
 
   context "when running symfony:composer:install with a given composer_bin" do
@@ -228,7 +228,7 @@ describe "Capifony::Symfony2 - symfony" do
     end
 
     it { should_not have_run('vendorDir=/var/www/current/vendor; if [ -d $vendorDir ] || [ -h $vendorDir ]; then cp -a $vendorDir /var/www/releases/20120927/vendor; fi;') }
-    it { should_not have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php\'') }
+    it { should_not have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php -d detect_unicode=Off -d allow_url_fopen=On\'') }
     it { should have_run(' sh -c \'cd /var/www/releases/20120927 && my_composer install --no-scripts --verbose --prefer-dist\'') }
   end
 
@@ -246,8 +246,8 @@ describe "Capifony::Symfony2 - symfony" do
       @configuration.find_and_execute_task('symfony:composer:dump_autoload')
     end
 
-    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php\'') }
-    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php composer.phar dump-autoload --optimize\'') }
+    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php -d detect_unicode=Off -d allow_url_fopen=On\'') }
+    it { should have_run(' sh -c \'cd /var/www/releases/20120927 && php -d detect_unicode=Off -d allow_url_fopen=On composer.phar dump-autoload --optimize\'') }
   end
 
   context "when running symfony:composer:dump_autoload with a given composer_bin" do
@@ -256,7 +256,7 @@ describe "Capifony::Symfony2 - symfony" do
       @configuration.find_and_execute_task('symfony:composer:dump_autoload')
     end
 
-    it { should_not have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php\'') }
+    it { should_not have_run(' sh -c \'cd /var/www/releases/20120927 && curl -s http://getcomposer.org/installer | php -d detect_unicode=Off -d allow_url_fopen=On\'') }
     it { should have_run(' sh -c \'cd /var/www/releases/20120927 && my_composer dump-autoload --optimize\'') }
   end
 

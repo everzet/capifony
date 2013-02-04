82f5037c9791bc96834c4269955885f301011df6
Piotr Antosik <antek88@gmail.com>
2013-02-04 16:24:27 +0100

    Fix Copying Composer from previous release
    Before:
    ```
    *** [err :: server] cp: `/home/domain/releases/20130204152210/composer.phar' and `/home/domain/releases/20130204152210/composer.phar' are the same file
    *** [deploy:update_code] rolling back
    ```
    
    After change work fine.

--- lib/symfony2/symfony.rb
+++ lib/symfony2/symfony.rb
@@ -97,9 +97,9 @@ namespace :symfony do
   namespace :composer do
     desc "Gets composer and installs it"
     task :get, :roles => :app, :except => { :no_release => true } do
-      if remote_file_exists?("#{previous_release}/composer.phar")
+      if remote_file_exists?("#{current_path}/composer.phar")
         capifony_pretty_print "--> Copying Composer from previous release"
-        run "#{try_sudo} sh -c 'cp #{previous_release}/composer.phar #{latest_release}/'"
+        run "#{try_sudo} sh -c 'cp #{current_path}/composer.phar #{latest_release}/'"
         capifony_puts_ok
       end
 

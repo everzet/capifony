b8ae37cc07b45275bf9753cf714a99a605d44d87
Jacob Richardson <jacob@jacobr.co.uk>
2013-03-05 17:53:23 +0000

    DB Refactor WIP.

--- lib/symfony2/database.rb
+++ lib/symfony2/database.rb
@@ -5,51 +5,54 @@ namespace :database do
   namespace :dump do
     desc "Dumps remote database"
     task :remote, :roles => :db, :only => { :primary => true } do
-      env       = fetch(:deploy_env, "remote")
-      filename  = "#{application}.#{env}_dump.#{Time.now.to_i}.sql.gz"
-      file      = "#{remote_tmp_dir}/#{filename}"
-      sqlfile   = "#{application}_dump.sql"
-      config    = ""
+      application_name = application.gsub(/\s+/, "_") # make application name safe
+      env              = fetch(:deploy_env, "remote")
+      filename         = "#{application_name}.#{env}_dump.#{Time.now.to_i}.sql.gz"
+      latest_filepath  = "backups/#{application_name}.#{env}_dump.latest.sql.gz"
+      backup_filepath  = "backups/#{filename}"
+      temp_filepath    = "#{remote_tmp_dir}/#{filename}"
+      config           = ""
 
       data = capture("#{try_sudo} cat #{current_path}/#{app_config_path}/#{app_config_file}")
       config = load_database_config data, symfony_env_prod
 
       case config['database_driver']
       when "pdo_mysql", "mysql"
-        data = capture("#{try_sudo} sh -c 'mysqldump -u#{config['database_user']} --host='#{config['database_host']}' --password='#{config['database_password']}' #{config['database_name']} | gzip -c > #{file}'")
+        data = capture("#{try_sudo} sh -c 'mysqldump -u#{config['database_user']} --host='#{config['database_host']}' --password='#{config['database_password']}' #{config['database_name']} | gzip -c > #{temp_filepath}'")
         puts data
       when "pdo_pgsql", "pgsql"
-        data = capture("#{try_sudo} sh -c 'pg_dump -U #{config['database_user']} #{config['database_name']} --clean | gzip -c > #{file}'")
+        data = capture("#{try_sudo} sh -c 'pg_dump -U #{config['database_user']} #{config['database_name']} --clean | gzip -c > #{temp_filepath}'")
         puts data
       end
 
       FileUtils.mkdir_p("backups")
-      get file, "backups/#{filename}"
+      get temp_filepath, backup_filepath # copy temporary file to backup path
       begin
-        FileUtils.ln_sf(filename, "backups/#{application}.#{env}_dump.latest.sql.gz")
+        FileUtils.ln_sf(filename, latest_filepath) # symlink newest backup path
       rescue Exception # fallback for file systems that don't support symlinks
-        FileUtils.cp_r("backups/#{filename}", "backups/#{application}.#{env}_dump.latest.sql.gz")
+        FileUtils.cp_r(backup_filepath, latest_filepath) # copy newest backup
       end
-      run "#{try_sudo} rm -f #{file}"
+      run "#{try_sudo} rm -f #{temp_filepath}"
     end
 
     desc "Dumps local database"
     task :local do
-      filename  = "#{application}.local_dump.#{Time.now.to_i}.sql.gz"
-      tmpfile   = "backups/#{application}_dump_tmp.sql"
-      file      = "backups/#{filename}"
-      config    = load_database_config IO.read("#{app_config_path}/#{app_config_file}"), symfony_env_local
-      sqlfile   = "#{application}_dump.sql"
+      filename         = "#{application_name}.local_dump.#{Time.now.to_i}.sql.gz"
+      latest_filepath  = "backups/#{application_name}.local_dump.latest.sql.gz"
+      backup_filepath  = "backups/#{filename}"
+      temp_filepath    = "backups/#{application_name}_dump_tmp.sql"
+      file             = "backups/#{filename}"
+      config           = load_database_config IO.read("#{app_config_path}/#{app_config_file}"), symfony_env_local
 
       FileUtils::mkdir_p("backups")
       case config['database_driver']
       when "pdo_mysql", "mysql"
-        `mysqldump -u#{config['database_user']} --password=\"#{config['database_password']}\" #{config['database_name']} > #{tmpfile}`
+        `mysqldump -u#{config['database_user']} --password=\"#{config['database_password']}\" #{config['database_name']} > #{temp_filepath}`
       when "pdo_pgsql", "pgsql"
-        `pg_dump -U #{config['database_user']} #{config['database_name']} --clean > #{tmpfile}`
+        `pg_dump -U #{config['database_user']} #{config['database_name']} --clean > #{temp_filepath}`
       end
 
-      File.open(tmpfile, "r+") do |f|
+      File.open(temp_filepath, "r+") do |f|
         gz = Zlib::GzipWriter.open(file)
         while (line = f.gets)
           gz << line
@@ -59,11 +62,11 @@ namespace :database do
       end
 
       begin
-        FileUtils.ln_sf(filename, "backups/#{application}.local_dump.latest.sql.gz")
+        FileUtils.ln_sf(filename, latest_filepath)
       rescue Exception # fallback for file systems that don't support symlinks
-        FileUtils.cp_r("backups/#{filename}", "backups/#{application}.local_dump.latest.sql.gz")
+        FileUtils.cp_r(backup_filepath, latest_filepath)
       end
-      FileUtils.rm(tmpfile)
+      FileUtils.rm(temp_filepath)
     end
   end
 
@@ -71,9 +74,9 @@ namespace :database do
     desc "Dumps remote database, downloads it to local, and populates here"
     task :to_local, :roles => :db, :only => { :primary => true } do
       env       = fetch(:deploy_env, "remote")
-      filename  = "#{application}.#{env}_dump.latest.sql.gz"
+      filename  = "#{application_name}.#{env}_dump.latest.sql.gz"
       config    = load_database_config IO.read("#{app_config_path}/#{app_config_file}"), symfony_env_local
-      sqlfile   = "#{application}_dump.sql"
+      sqlfile   = "#{application_name}_dump.sql"
 
       database.dump.remote
 
@@ -93,9 +96,9 @@ namespace :database do
 
     desc "Dumps local database, loads it to remote, and populates there"
     task :to_remote, :roles => :db, :only => { :primary => true } do
-      filename  = "#{application}.local_dump.latest.sql.gz"
+      filename  = "#{application_name}.local_dump.latest.sql.gz"
       file      = "backups/#{filename}"
-      sqlfile   = "#{application}_dump.sql"
+      sqlfile   = "#{application_name}_dump.sql"
       config    = ""
 
       database.dump.local
